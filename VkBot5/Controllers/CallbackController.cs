1d4da8cb4702f1989b508ab32a3dd10abd818b90
Maxim Fomenko <maxim112111@gmail.com>
2021-03-13 11:45:50 +0300

    vk

---
+++ VkBot5/Controllers/CallbackController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VkBot5.Models;
+using VkNet.Abstractions;
+using VkNet.Model;
+
+namespace VkBot5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly IVkApi vkApi;
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.vkApi = vkApi;
+        }
+        [HttpPost]
+        public IActionResult Callback([FromBody] IncomeMessage incomeMessage)
+        {
+            switch (incomeMessage.Type)
+            {
+                case "confirmation":
+                    return Ok(configuration["Config:ConfirmMessage"]);
+                case "message_new":
+                    {
+                        var msg = Message.FromJson(new VkNet.Utils.VkResponse(incomeMessage.Object));
+                        vkApi.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = "соси хуй"
+                        });
+                        break;
+                    }
+            }
+            return Ok("ok");
+        }
+    }
+}
---
+++ VkBot5/Models/IncomeMessage.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VkBot5.Models
+{
+    [Serializable]
+    public class IncomeMessage
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("object")]
+        public string Object { get; set; }
+        [JsonProperty("group_id")]
+        public string GroupId { get; set; }
+    }
+}
